142aa23ce7e36b8b8420b40e84005d0a2d3b743b
Hamza Hameed <hh0718@yahoo.com>
2021-01-21 17:07:14 -0600

    updated how healh upgrades work, updated changing between scenes

--- Project/src/Autoload/PlayerData.gd
+++ Project/src/Autoload/PlayerData.gd
@@ -15,6 +15,8 @@ var health: = 100 setget set_health
 var shields: = 0 setget set_shields
 var max_shields: = 25 setget set_max_shields
 var has_shields: = false setget set_give_shields
+var healthUpgrades = [500, 1000, 2000, 4000]
+var curLevel = ["res://src/Levels/Level02.tscn","res://src/Levels/LevelTemplate.tscn"]
 
 func reset() -> void:
 	gold = 0
--- Project/src/Screens/UpgradeScreen.gd
+++ Project/src/Screens/UpgradeScreen.gd
@@ -1,15 +1,25 @@
 extends Control
 
 onready var gold: Label = get_node("GoldLabel")
+onready var healthUpgrade: Label = get_node("HealthLabel")
+onready var changeSceneButton: Button = get_node("ChangeSceneButton")
 
 func _ready() -> void:
+	changeSceneButton.next_scene_path = PlayerData.curLevel.front()
 	gold.text = gold.text % [PlayerData.gold]
+	healthUpgrade.text = "$ %s" % PlayerData.healthUpgrades.front()
 
 func update() -> void:
-	gold.text = "Gold: %s" % PlayerData.gold
+	gold.text = "Your Gold: %s" % PlayerData.gold
+	healthUpgrade.text = "$ %s" % PlayerData.healthUpgrades.front()
 
 
 func _on_UpgradeHealth_button_up():
-	PlayerData.gold -= 100
-	PlayerData.max_health += 25
+	if(PlayerData.gold >= PlayerData.healthUpgrades.front()):
+		PlayerData.gold -= PlayerData.healthUpgrades.front()
+		PlayerData.healthUpgrades.pop_front()
+		PlayerData.max_health += 25
 	update()
+	
+func _on_ChangeSceneButton_button_up():
+	PlayerData.curLevel.pop_front()

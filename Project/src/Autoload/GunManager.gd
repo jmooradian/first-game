0432e349ba05bb3ddf73d00b6f8379aeb5773ccf
jmooradian <juniorbirdman52@gmail.com>
2021-01-28 19:32:55 -0600

    Added guns into GunManager and copied upgrade arrays to UpgradesManager

--- Project/src/Autoload/GunManager.gd
+++ Project/src/Autoload/GunManager.gd
@@ -7,9 +7,33 @@ var pistol = {
 	hasgun = false
 }
 
+var shotgun = {
+	damage = 25,
+	numbullets = 10,
+	max_numbullets = 10,
+	hasgun = false
+}
+
+var rifle = {
+	damage = 40,
+	numbullets = 5,
+	max_numbullets = 5,
+	hasgun = false
+}
+
+var rocketlauncher = {
+	damage = 100,
+	numbullets = 3,
+	max_numbullets = 3,
+	hasgun = false
+}
+
 
 var guns = {
-	"pistol": pistol
+	"pistol": pistol,
+	"shotgun": shotgun,
+	"rifle": rifle,
+	"rocketlauncher": rocketlauncher
 }
 
 
---
+++ Project/src/Autoload/UpgradeManager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var healthUpgrades = [500, 1000, 2000, 4000]
+var speedUpgrades = [500, 1000, 2000, 4000]
+var jumpUpgrades = [500, 1000, 2000, 4000]
+var gunUpgrades = [500, 1000, 2000, 4000]
+var shieldUpgrades = [500, 1000, 2000, 4000]

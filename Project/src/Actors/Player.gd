f916d7bc357dc639056321a441f51e03cc00ce4e
jmooradian <juniorbirdman52@gmail.com>
2021-02-08 15:15:22 -0600

    Added arms and legs for the player. Added walking and idle animations for the player

--- Project/src/Actors/Player.gd
+++ Project/src/Actors/Player.gd
@@ -2,10 +2,11 @@ extends Actor
 
 export var stomp_impulse: = 1000.0
 
-onready var larrow = $leftarrow
-onready var rarrow = $rightarrow
-onready var p_sprite = $Sprite
+onready var p_sprite = $player
+onready var p_anim_player = $player/AnimationPlayer
 var BULLET_SCENE = preload("res://src/Objects/Bullet.tscn")
+
+var looking = "right"
 
 var respawn_loc: = Vector2(240, -100)
 var lives_left: = 3
@@ -52,9 +53,12 @@ func _physics_process(_delta):
 	var is_jump_interupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
 	if direction.x == 1.0:
-		p_sprite.flip_h = false
+		looking = "right"
+		p_anim_player.play("WalkingRight")
 	elif direction.x == -1.0:
-		p_sprite.flip_h = true
+		looking = "left"
+		p_anim_player.play("WalkingLeft")
+	
 	if Input.is_action_just_released("shoot") and PlayerData.get_gun() != "" and GunManager.guns.get(PlayerData.get_gun()).numbullets > 0:
 		GunManager.guns.get(PlayerData.get_gun()).numbullets -= 1
 		var bullet = BULLET_SCENE.instance()
@@ -65,7 +69,7 @@ func _physics_process(_delta):
 			bullet.set_position(get_node("bulletPosL").get_global_position())
 			bullet.speedDir = -bullet.speedDir
 		else:
-			if larrow.visible == true:
+			if looking == "left":
 				bullet.set_position(get_node("bulletPosL").get_global_position())
 				bullet.speedDir = -bullet.speedDir
 			else:
@@ -125,4 +129,11 @@ func damage(value: int):
 func die() -> void:
 	PlayerData.set_deaths(PlayerData.get_deaths() + 1)
 	queue_free()
+
+func flip(value: bool):
+	p_sprite.get_node("torso").flip_h = value
+	p_sprite.get_node("leftleg").flip_h = value
+	p_sprite.get_node("leftarm").flip_h = value
+	p_sprite.get_node("rightleg").flip_h = value
+	p_sprite.get_node("rightarm").flip_h = value
 #--------------------------------------------------------------------
